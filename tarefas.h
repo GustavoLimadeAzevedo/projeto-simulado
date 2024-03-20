24570c983c0e9d7c47097bbeb4af387e3317e723
GustavoLimadeAzevedo <122912072+GustavoLimadeAzevedo@users.noreply.github.com>
2024-03-20 19:03:07 +0000

    teste erro

---
+++ tarefas.h
@@ -0,0 +1,10 @@
+typedef struct Tarefa{
+ int prioridade;
+ char categoria[100];
+ char descricao[300];
+
+} Tarefa;
+
+typedef enum Erro{
+OK, ABRIR, FECHAR, LER, ESCREVER, MAX_TAREFAS, SEM_TAREFAS, NAO_ENCONTRADO
+} Erro;
